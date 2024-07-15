cd6da86cfe01feb0df8c501c31652dd0288e8de2
Sandaruwani11 <155408597+Sandaruwani11@users.noreply.github.com>
2024-07-15 15:50:46 +0530

    q2.scala

--- q2.scala
+++ q2.scala
@@ -21,17 +21,31 @@ object LibraryManagement {
     }
   }
 
-  def displayLibrary(): Unit = {
+  def display(): Unit = {
     println("Current Library Collection:")
     library.foreach(book => println(s"${book._1} by ${book._2}, ISBN: ${book._3}"))
   }
 
+  def searchByAuthor(author: String): Unit = {
+    val books = library.filter(book => book._2 == author)
+    if (books.nonEmpty) {
+      println(s"Books by $author:")
+      books.foreach { book =>
+        println(s"${book._1}, ISBN: ${book._3}")
+      }
+    } else {
+      println(s"No books found by $author")
+    }
+  }
+
   def main(args: Array[String]): Unit = {
   
-    displayLibrary()
+    display()
     addBook("Book4", "Author1", "ISBN4")
-    displayLibrary()
+    display()
     removeBook("ISBN2")
-    displayLibrary()
+    display()
+    searchByAuthor(Author1)
+    
   }
 }
